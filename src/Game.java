3bfb4b41a08c598f21e9502252dc6e0411b46d64
rootrc <rootrc123@gmail.com>
2023-07-05 20:31:47 -0400

    h

--- src/Game.java
+++ src/Game.java
@@ -2,20 +2,12 @@ import java.awt.Color;
 import javax.swing.*;
 
 public class Game {  
-    /* To Do List
-     * Finish Level 13
-     * Do extra door laser graphics
-     * Do player tries to walk into door graphics (optional)
-     * Do white connecter stuff (going to be really really annoying
-     * Add the rest of the levels :D
-     */
     static Level level;
     static Panel panel;
     static JFrame frame;
     static int gameState = 0;
     static boolean loaded = false;
     public static void main(String[] args) {  
-        
         frame = new JFrame ();
         frame.setTitle("Game");
         frame.setBackground(Color.black);
